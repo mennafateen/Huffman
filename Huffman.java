f06072842b3370d2cd2eb2bcec801f28583fab27
Menna Fateen <menna.fateen@gmail.com>
2016-11-10 21:24:33 +0200

    done

--- Huffman.java
+++ Huffman.java
@@ -1,22 +1,45 @@
 /**
  * Created by menna on 11/2/2016.
  */
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.*;
+import java.io.*;
 import java.util.*;
-public class Huffman {
+import java.util.Scanner;
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Huffman extends Application {
 
     public static void main(String args[]) {
-        Scanner cin = new Scanner(System.in);
-        String original = cin.nextLine();
-        Compress(original);
-        File f = new File("table.txt");
-        deCompress(Compress(original), f);
+        launch(args);
     }
 
-    public static String Compress(String original) {
+    public static File Compress(String path) {
+        String original = "";
+        try {
+            original = new Scanner(new File(path)).useDelimiter("\\Z").next(); // convert everything in file into one string -> content
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
         String result = "";
         HashMap<Character, Integer> freq = calculateFrequency(original);
        // PriorityQueue<Node> nodes = calculateProbability(freq, original.length());
@@ -35,7 +58,12 @@ public class Huffman {
             result += table.get(original.charAt(i));
         }
         System.out.println(result);
-        File file = new File("table.txt");
+
+        File originFile = new File(path);
+        String dir = originFile.getParent();
+
+        File file = new File(dir + "\\table.txt");
+
         String write = "";
         for (Map.Entry<Character, String> entry : table.entrySet()) {
             Character key = entry.getKey();
@@ -46,17 +74,43 @@ public class Huffman {
             write += "\n";
             //System.out.println("key, " + key + " value " + value);
         }
-        try (PrintWriter out = new PrintWriter("table.txt")) { // make a new compressed file in same working dir
+        try (PrintWriter out = new PrintWriter(dir + "\\table.txt")) { // make a new compressed file in same working dir
             out.println(write); // write tags
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result;
+
+
+        File compressed = new File(dir + "\\compressed.txt"); // change to / on linux
+
+        try(PrintWriter out = new PrintWriter(dir + "\\compressed.txt")) { // make a new compressed file in same working dir
+            out.println(result); // write tags
+            out.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return compressed;
+      //  return result;
     }
 
 
-    public static String deCompress(String code, File table) {
+    public static File deCompress(String path) {
+        String code = "";
+        try {
+            code = new Scanner(new File(path)).useDelimiter("\\Z").next(); // convert everything in file into one string -> content
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File originFile = new File(path);
+        String dir = originFile.getParent();
+
+        File table = new File(dir + "\\table.txt");
+
         String result = "";
         HashMap<String, Character> reversedTable = new HashMap<>();
         try(Scanner cin = new Scanner(table)) {
@@ -82,7 +136,18 @@ public class Huffman {
         }
         System.out.println(result);
 
-        return result;
+        File decompressed = new File(dir + "\\decompressed.txt");
+
+        try(PrintWriter out = new PrintWriter(dir + "\\decompressed.txt")) { // make a decompressed file in same working dir
+            out.println(result);
+            out.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return decompressed;
+      //  return result;
     }
 
 
@@ -112,6 +177,103 @@ public class Huffman {
             Nodes.add(node);
         }
         return Nodes;
+
+    }
+
+
+    // GUI
+
+    private Desktop desktop = Desktop.getDesktop(); // opening files like this doesnt work on linux
+    private void openFile(File file) {
+        try {
+            desktop.open(file); // use processbuilder for linux
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    Button browse, compress, decompress;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("LZ77 Compressor/Decompressor Tool");
+        Label home = new Label("Choose file to compress/decompress:");
+        TextField fillWithPath = new TextField();
+        fillWithPath.setMaxWidth(200);
+        browse = new Button("Browse");
+        FileChooser choose = new FileChooser();
+        browse.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser.ExtensionFilter extFilter =
+                        new FileChooser.ExtensionFilter("TEXT files (*.txt)", "*.txt"); // only choose text files
+                choose.getExtensionFilters().add(extFilter);
+
+                // show open file dialog
+                File file = choose.showOpenDialog(primaryStage);
+                fillWithPath.setText(file.getAbsolutePath()); // extension filter needed here
+
+            }
+        });
+        VBox layout1  = new VBox(20);
+        layout1.setAlignment(Pos.CENTER);
+        // StackPane layout1 = new StackPane();
+        compress = new Button("Compress");
+        decompress = new Button("Decompress");
+
+        compress.setOnAction(e -> {
+            if (fillWithPath.getCharacters().toString().isEmpty()) { // FILE NOT CHOSEN
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Please choose a file.");
+                alert.show();
+            }
+            else if (!new File(fillWithPath.getCharacters().toString()).isFile()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Please choose a valid path");
+                alert.show();
+            }
+            else {
+                File o = Compress(fillWithPath.getText());
+                openFile(o);
+            }
+
+
+        });
+        decompress.setOnAction(e -> {
+            if(fillWithPath.getCharacters().toString().isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Please choose a file.");
+                alert.show();
+            }
+            else if (!new File(fillWithPath.getCharacters().toString()).isFile()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Please choose a valid path");
+                alert.show();
+            }
+            else {
+                File d = deCompress(fillWithPath.getText());
+                openFile(d);
+            }
+        });
+
+
+        layout1.getChildren().addAll(home, fillWithPath, browse, compress, decompress);
+        Scene scene = new Scene(layout1, 300, 250);
+        layout1.setBackground(new Background(new BackgroundFill(Color.LAVENDERBLUSH, CornerRadii.EMPTY, Insets.EMPTY))); //THIS IS SO FUN OMG - LOVE THE COLOR NAMES
+        compress.setFont(Font.font("Lucida Grande")); // just for fun :) عشان انا فاضية
+        decompress.setFont(Font.font("Lucida Grande"));
+        browse.setFont(Font.font("Lucida Grande"));
+        fillWithPath.setFont(Font.font("Lucida Grande"));
+        home.setFont(Font.font("Lucida Grande"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
 
     }
 
